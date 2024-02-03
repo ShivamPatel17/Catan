68b3f873db5ff0a3c2ca8b154882e51cf5943519
Shivam <shivam.patel@dailypay.com>
2024-02-03 14:51:02 -0500

    initial website with css, js, and html

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+// https://www.alexedwards.net/blog/serving-static-sites-with-go
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
