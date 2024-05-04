12f4ffc6bdbeb5b7ddacd23101801d0c09494452
Shivam <shivam.patel@dailypay.com>
2024-05-04 10:11:25 -0400

    dice

---
+++ frontend/public/cards.js
@@ -0,0 +1,48 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 600 },
+        debug: false
+    }
+},
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+var cursors;
+var dice;
+var die;
+
+var dieDim = 68
+
+
+function preload() { 
+
+  this.load.spritesheet('redDie', 'boardgamePack_v2/Spritesheets/diceRed_border.png', {frameWidth: dieDim, frameHeight: dieDim})
+}
+
+function create(){
+  dice = this.physics.add.staticGroup();
+  die = dice.create(300, 300, 'redDie')
+
+
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update(){
+  if (cursors.left.isDown) { 
+    roll()
+  }
+}
+
+function roll() { 
+  die.disableBody(true, true)
+}
