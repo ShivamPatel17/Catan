fb14512ad86e48ed681f23096a809a7badf05641
Shivam <shivam.patel@dailypay.com>
2024-05-12 20:38:45 -0400

    space bar rolls die :)

--- frontend/cards.js
+++ frontend/cards.js
@@ -1,47 +1,63 @@
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-        debug: true
+// the game itself
+let game;
+
+// global object with game options
+let gameOptions = {
+
+    // card width, in pixels
+    diceWidth: 64,
+
+    // card height, in pixels
+    diceHeight: 64,
+
+    // card scale. 1 = original size, 0.5 half size and so on
+    cardScale: 0.8
+}
+window.onload = function() {
+    let gameConfig = {
+        type: Phaser.AUTO,
+        backgroundColor: 0x4488aa,
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            parent: "thegame",
+            width: 800,
+            height: 600
+        },
+        scene: playGame
     }
-},
-  scene: {
-      preload: preload,
-      create: create,
-      update: update
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+}
+
+class playGame extends Phaser.Scene {
+    constructor() {
+        super("PlayGame");
+        this.redDieNum = 1
+    }
+
+    preload() {
+      // loading the sprite sheet with all cards
+      this.load.spritesheet("redDie", "boardgamePack_v2/Spritesheets/diceRed.png", {
+          frameWidth: gameOptions.diceWidth,
+          frameHeight: gameOptions.diceHeight
+      });
+    }
+    
+    create() {
+      // Add the dice sprite to the scene
+      this.die = this.add.sprite(400, 300, 'redDie').setInteractive();
+
+      // Add a keyboard listener for the spacebar
+      this.input.keyboard.on('keydown-SPACE', this.rollDie, this);
   }
-};
 
-var game = new Phaser.Game(config);
-var cursors;
-var dice;
-var die;
-
-var dieDim = 68
-
-
-function preload() { 
-
-  this.load.spritesheet('redDie', 'boardgamePack_v2/Spritesheets/diceRed_border.png', {frameWidth: dieDim, frameHeight: dieDim})
-}
-
-function create(){
-  dice = this.physics.add.staticGroup();
-  die = dice.create(300, 300, 'redDie')
-
-
-  cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update(){
-  if (cursors.left.isDown) { 
-    roll()
+  rollDie() {
+      // Generate a random dice face between 0 and 5 (for a six-faced dice)
+      const randomFace = Phaser.Math.Between(0, 5);
+      // Set the dice sprite to display the randomly selected face
+      this.die.setFrame(randomFace);
   }
+
 }
 
-function roll() { 
-  die.disableBody(true, true)
-}
